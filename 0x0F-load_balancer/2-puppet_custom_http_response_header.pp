a54935515e8bf7ada9b706af2ce59587802471ff
elgharbi <elgharbiay@gmail.com>
2023-10-03 02:22:29 +0100

    fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,30 @@
-# config nginx
-# Install Nginx package
-class { 'nginx':
-  ensure => 'installed',
+#automate the task of creating a custom HTTP header response
+$hostname = $::hostname
+exec { 'update server':
+  command  => 'apt-get update',
+  user     => 'root',
+  provider => shell,
 }
 
-file_line { 'add_http_header':
-  ensure  => present,
-  path    => '/etc/nginx/sites-available/default',
-  line    => 'add_header X-Served-By $::hostname;',
-  after   => 'server_name _;',
-  require => Class['nginx'],
-  notify  => Service['nginx'],
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt'
 }
 
 service { 'nginx':
   ensure  => 'running',
   enable  => true,
-  require => File_line['add_http_header'],
+  require => Package['nginx']
+}
+
+file_line { 'add HTTP header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => "\n\tadd_header X-Served-By \"${hostname}\";"
+}
+
+exec { 'restart service':
+    command  => 'sudo service nginx restart',
+    provider => shell,
 }
