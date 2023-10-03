b364800e38369f62f3153ba27c528818ffc4493a
elgharbi <elgharbiay@gmail.com>
2023-10-03 22:44:41 +0100

    fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,19 +2,14 @@
 exec {'update':
   command => '/usr/bin/apt-get update',
 }
-
-package { 'nginx':
-  ensure   => present,
-  provider => 'apt'
+-> package { 'nginx':
+  ensure   => installed,
 }
-
-file_line { 'add HTTP header':
-  ensure => 'present',
+-> file_line { 'add HTTP header':
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _;',
   line   => "\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-exec { 'restart service':
+-> exec { 'restart service':
     command  => '/usr/sbin/service nginx restart',
 }
