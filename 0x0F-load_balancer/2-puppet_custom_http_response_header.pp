4cbd92d1b201a4697a2039c61cf4fcc9c100f4b6
elgharbi <elgharbiay@gmail.com>
2023-10-03 02:32:51 +0100

    ooo

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,6 @@
 #automate the task of creating a custom HTTP header response
-$hostname = $::hostname
-exec { 'update server':
-  command  => 'apt-get update',
-  user     => 'root',
-  provider => shell,
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
@@ -11,20 +8,13 @@ package { 'nginx':
   provider => 'apt'
 }
 
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Package['nginx']
-}
-
 file_line { 'add HTTP header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _;',
-  line   => "\n\tadd_header X-Served-By \"${hostname}\";"
+  line   => "\n\tadd_header X-Served-By \"${hostname}\";",
 }
 
 exec { 'restart service':
-    command  => 'sudo service nginx restart',
-    provider => shell,
+    command  => '/usr/sbin/service nginx restart',
 }
