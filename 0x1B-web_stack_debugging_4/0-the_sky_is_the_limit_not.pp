5b7752b636a45ccb58ec21148c20a9b113015bb6
elgharbi <elgharbiay@gmail.com>
2023-11-16 22:24:03 +0100

    fixed

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # fixes a file
 exec {'sets file limite for nginx':
-  command => 'sed -i "s/15/2000/g" /etc/default/nginx',
+  command => "sed -i 's/15/2000/g' /etc/default/nginx && sudo service nginx restart",
   path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
   onlyif  => 'test -f /etc/default/nginx'
 }
